73f601d73a61defa4b150f9888fb2ca1204d8180
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-21 15:41:14 -0500

    Complete project requirements -- Booking flow redirect and index pages

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def format_flight_date(date)
-    date.strftime('%m/%d/%Y')
-  end
-
-  def format_flight_time(time)
-    time.strftime('%I:%M%P')
-  end
-end
--- db/migrate/20240317205434_add_duration_to_flights.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToFlights < ActiveRecord::Migration[7.1]
-  def change
-    add_column :flights, :duration, :integer
-  end
-end
